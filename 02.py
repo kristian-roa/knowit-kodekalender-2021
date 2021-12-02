c78a3bc846145f73fc85a11b161228580825f3e7
krisrgra <krisrgra@uio.no>
2021-12-02 13:45:16 +0100

    Luke 2 forbedret

--- 02.py
+++ 02.py
@@ -1,37 +1,30 @@
 import requests
-import csv
-import re
 from math import radians, sin, cos, acos
+import parse
 
 
 def main():
     url = 'https://julekalender-backend.knowit.no/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBOUT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--7bb23c39ab7eb5b367e3b0841b86e0667756397f/cities.csv?disposition=inline'
     data = requests.get(url).content.decode('utf-8')
-    data = csv.reader(data.splitlines(), delimiter=',')
-    cities = {city[0]: point_to_coord(city[1]) for city in data}
-    del cities['city']
 
-    # BEGIN
-    POLE = (0, 90)
-    location = POLE
+    pattern = parse.compile('{city},Point({x:f} {y:f})')
+    cities = set((match['x'], match['y']) for line in data.splitlines() if (match := pattern.parse(line)))
+
+    location = POLE = (0, 90)
     distance = 0
 
     while cities:
-        closest = min(cities.items(), key=lambda x: dist(location, x[1]))
-        del cities[closest[0]]
-        distance += dist(location, closest[1])
-        location = closest[1]
+        closest = min(cities, key=lambda x: dist(location, x))
+        cities.remove(closest)
+        distance += dist(location, closest)
+        location = closest
 
     distance += dist(location, POLE)
     print(round(distance))
 
 
-def point_to_coord(point):
-    return tuple(float(x) for x in re.findall(r'[-+]?\d*\.\d+|\d+', point))
-
-
 def dist(a, b):
-    lon1, lat1, lon2, lat2 = map(radians, [a[0], a[1], b[0], b[1]])
+    lon1, lat1, lon2, lat2 = map(radians, [*a, *b])
     return 6371 * (acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon1 - lon2)))
 
 
