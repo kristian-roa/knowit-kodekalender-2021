692938500859663c4329ecba54a78a0f1749fd18
krisrgra <krisrgra@uio.no>
2021-12-03 14:36:37 +0100

    Luke 3 forbedret

--- 03.py
+++ 03.py
@@ -6,12 +6,12 @@ def main():
     url = 'https://julekalender-backend.knowit.no/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBOdz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--31fa0c541c69eeb9063ccfc56e686f4768662004/input.txt?disposition=inline'
     neighbours = requests.get(url).text
 
-    vals = [{'J': 1, 'N': -1}[c] for c in neighbours]
+    vals = [0] + [{'J': 1, 'N': -1}[c] for c in neighbours]
     last_idx, size, idx = {}, 0, 0
     for i, cs in enumerate(np.cumsum(vals)):
         if cs in last_idx:
             if (j := i - last_idx[cs]) > size:
-                size, idx = j, last_idx[cs] + 1
+                size, idx = j, last_idx[cs]
         else:
             last_idx[cs] = i
 
