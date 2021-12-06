42a65f39b4ce211c824943b362278bef8f53c897
krisrgra <krisrgra@uio.no>
2021-12-06 16:28:50 +0100

    Luke 6 løst

---
+++ 06.py
@@ -0,0 +1,42 @@
+import requests
+import numpy as np
+
+
+def main():
+    url = 'https://julekalender-backend.knowit.no/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBallDIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--20b29549a475416a15aa81ff11b00da4c4103e67/pakker.txt?disposition=inline'
+    gifts = [(int((x := s.split(','))[0]), int(x[1])) for s in requests.get(url).text.splitlines()]
+
+    stack = np.full((1, 21), True)
+    fall = 0
+
+    for x, length in gifts:
+        # insert new row on top
+        if stack[0, :].any(): stack = np.concatenate((np.full((1, 21), False), stack))
+
+        # find row to insert gift
+        idx = 0
+        for i, row in enumerate(stack[:-1, :]):
+            if row[x:x+length].any(): break
+            idx = i
+
+        # support under middle ?
+        mid = length // 2
+        if length % 2 == 0:
+            mid_support = stack[idx+1, x+mid-1:x+mid+1].all()
+        else:
+            mid_support = stack[idx+1, x+mid]
+
+        # support on left and right side?
+        left = stack[idx+1, x:x+mid].any()
+        right = stack[idx+1, x+length-mid:x+length].any()
+
+        if (left and right) or mid_support:
+            stack[idx, x:x+length] = True
+        else:
+            fall += 1
+
+    print(fall)
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# knowit-kodekalender-2021
+# Knowit kodekalender 2021
+
+
+Luke | Hva | Løst
+:------------ | :------------- | :-------------
+1 | Strengtall | :heavy_check_mark:
+2 | Nissens rute | :heavy_check_mark:
+3 | Snille og slemme barn | :heavy_check_mark:
+4 | Sneglulf i Atlanteren | :heavy_check_mark:
+5 | Slektstre | :heavy_check_mark:
+6 | Pakkestabel | :heavy_check_mark:
+7 |  | :x:
+8 |  | :x:
+9 |  | :x:
+10 |  | :x: 
+11 |  | :x:
+12 |  | :x:
+13 |  | :x:
+14 |  | :x:
+15 |  | :x:
+16 |  | :x:
+17 |  | :x:
+18 |  | :x:
+19 |  | :x:
+20 |  | :x:
+21 |  | :x:
+22 |  | :x:
+23 |  | :x:
+24 |  | :x:
